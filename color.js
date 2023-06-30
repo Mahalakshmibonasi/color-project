dc43d91c9d18ec9497d439092be09e57d874b3f2
Mahalakshmibonasi <106964851+Mahalakshmibonasi@users.noreply.github.com>
2023-06-30 19:16:49 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,25 @@
+let box = document.getElementsByClassName("box");
+
+let one=document.getElementById("red");
+one.onclick= function(){
+    box[0].style.backgroundColor= "red";
+};
+
+const two=document.getElementById("pink");
+two.onclick= function(){
+    box[0].style.backgroundColor= "pink";
+};
+
+const three=document.getElementById("blue");
+three.onclick= function(){
+    box[0].style.backgroundColor= "blue";
+};
+
+const four=document.getElementById("green");
+four.onclick= function(){
+    box[0].style.backgroundColor= "green";
+};
+
+
+
+    
